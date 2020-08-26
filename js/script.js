efd6401e5ffaa807334462e6947192010b4030e8
nfed-india <info.nfed@gmail.com>
2020-08-26 11:30:30 +0530

    Added footer, accordion for preamble, Publication Guidlines nav tab.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-function w3_open() {
-  document.getElementById("content").style.marginLeft = "25%";
-  document.getElementById("sidebar").style.width = "25%";
-  document.getElementById("sidebar").style.display = "block";
-  document.getElementById("openNav").style.display = 'none';
-}
-function w3_close() {
-  document.getElementById("content").style.marginLeft = "0%";
-  document.getElementById("sidebar").style.display = "none";
-  document.getElementById("openNav").style.display = "inline-block";
+function accordion(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
