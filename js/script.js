88bb920f77a614e444f1bb7dd16c2d53a4e53e12
GJaswin <jaswinkumarnr@gmail.com>
2021-07-21 19:39:54 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function accordion(id) {
   var x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-  } else { 
+  } else {
     x.className = x.className.replace(" w3-show", "");
   }
 }
